c437ded746abc4e70967b4e720dfbaadcc72bc3c
Brian Koser <dev@briankoser.com>
2016-08-30 16:05:23 -0500

    Add roster columns

--- src/components/Roster.vue
+++ src/components/Roster.vue
@@ -7,18 +7,26 @@
 
     <table v-for="section in rosters(teamSelected)">
       <tr>
-        <th>{{ section.position }}</th>
-        <th>Round</th>
+        <th rowspan="2" class="position">{{ section.position }}</th>
+        <th rowspan="2">Round</th>
+        <th colspan="2">Overall</th>
+        <th colspan="2">Position</th>
+        <th rowspan="2">Bye</th>
+      </tr>
+      <tr>
+        <th>Rank</th>
         <th>Pick</th>
         <th>Rank</th>
-        <th>Bye</th>
+        <th>Pick</th>
       </tr>
       <tr v-for="pick in section.picks" track-by="$index" :class="{ 'empty-row': !pick.player }">
-        <td>{{ pick.player ? pick.player.name : '' }}</td>
-        <td>{{ pick.round }}</td>
-        <td>{{ pick.numberOverall }}</td>
-        <td>{{ pick.ranking }}</td>
-        <td>{{ pick.bye }}</td>
+        <td class="player">{{ pick.player ? pick.player.name : '' }}</td>
+        <td class="round">{{ pick.round }}</td>
+        <td class="rank">{{ pick.rankingOverall }}</td>
+        <td class="pick">{{ pick.numberOverall }}</td>
+        <td class="rank"></td>
+        <td class="pick"></td>
+        <td class="bye">{{ pick.bye }}</td>
       </tr>
     </table>
   </aside>
@@ -54,26 +62,43 @@ aside {
   padding: 20px;
 }
 
-th:first-child {
+th {
+  font-size: 0.7em;
+  padding: 0 10px;
+}
+
+th[colspan="2"] {
+  border-bottom: none;
+  text-align: center;
+}
+
+tr:not(:first-child) th {
+  font-size: 0.55em;
+}
+
+th.position {
   font-size: 1.25em;
 }
 
-th:not(:first-child) {
-  font-size: 0.75em;
+td {
+  padding: 2px 10px;
 }
 
 td:first-child {
+  padding: 2px 0;
   width: 100%;
 }
 
-td:not(:first-child) {
+td.round,
+td.rank,
+td.bye {
   text-align: right;
 }
 
 .empty-row {
   background-color: #bdbdbd;
   border-top: solid #424242 10px;
-  opacity: 0.6;
   height: 1.4em;
+  opacity: 0.6;
 }
 </style>
