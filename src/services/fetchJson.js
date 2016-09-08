de459174d2b4355d010b9aa42e228903064ac138
Brian Koser <brianmkoser@gmail.com>
2016-09-07 21:32:25 -0500

    Add adp rankings

--- src/services/fetchJson.js
+++ src/services/fetchJson.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export default {
   fetchPlayers (success, fail) {
-    let rankings = Vue.http.get('/static/adpRankings.json')
+    let rankings = Vue.http.get('/static/adpRankings-16-teams-2-qb-ppr.json')
     let players = Vue.http.get('/static/players.json')
     let byes = Vue.http.get('/static/byes.json')
 
