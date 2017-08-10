04d27c0efa69beb8e67926c3bcf09a2e6f5e1145
Brian Koser <dev@briankoser.com>
2017-08-10 15:20:34 -0500

    Add todo comment

--- src/mixins/rankings.js
+++ src/mixins/rankings.js
@@ -12,12 +12,13 @@ export default {
     },
     setUserRankings: (rankings) => {
       let players = JSON.parse(JSON.stringify(rankings))
+
       return players.map((player, i, arr) => {
         player.userRanking.overall = i + 1
         player.userRanking.position = arr
             .filter(x => x.positionKey === player.positionKey)
             .findIndex(x => x.id === player.id) + 1
-        player.userRanking.stdDev = math.std(player.ranking.overall, player.userRanking.overall)
+        player.userRanking.stdDev = math.std(player.ranking.overall, player.userRanking.overall) // todo: fix std dev calculation
         return player
       })
     }
