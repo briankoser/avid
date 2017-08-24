89c4cb5618c08e808ccd7731d74b93822ec9f26b
Brian Koser <dev@briankoser.com>
2017-08-24 11:09:45 -0500

    Implement target tag

--- src/components/Rankings.vue
+++ src/components/Rankings.vue
@@ -12,10 +12,10 @@
             <td>{{ player.positionKey }}</td>
             <td>{{ player.name }}</td>
             <td class="tags">
-                <div class="tagIconParent">
+                <div class="tagIconParent" v-if="isTarget(player.id)" title="Target">
                     <svg class="tagIcon target" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M388.4 123.6l3.8-42.2c0.4-4.1 2.2-8 5.1-10.9l52.9-52.9c3.4-3.4 9-3.4 12.5 0l31.7 31.7c3.4 3.4 3.4 9 0 12.5l-52.9 52.9c-2.9 2.9-6.8 4.7-10.9 5.1L388.4 123.6z" class="a"/><circle cx="211.9" cy="300.1" r="211.9" class="b"/><circle cx="211.9" cy="300.1" r="158.9" class="c"/><circle cx="211.9" cy="300.1" r="105.9" class="b"/><circle cx="211.9" cy="300.1" r="53" class="c"/><path d="M211.9 309c-2.3 0-4.5-0.9-6.2-2.6 -3.4-3.4-3.4-9 0-12.5l291.3-291.3c3.4-3.4 9-3.4 12.5 0 3.4 3.4 3.4 9 0 12.5l-291.3 291.3C216.4 308.1 214.1 309 211.9 309z" fill="#464655"/><path d="M509.4 2.6c-3.4-3.4-9-3.4-12.5 0l-15.1 15.1 12.5 12.5 15.1-15.1C512.9 11.6 512.9 6 509.4 2.6z" class="a"/></svg>
                 </div>
-                <div class="tagIconParent" v-if="isDomeKicker(player.positionKey, player.nflTeam.name)">
+                <div class="tagIconParent" v-if="isDomeKicker(player.positionKey, player.nflTeam.name)" title="Dome">
                     <svg class="tagIcon dome" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 175.9"><path d="m247.7 175.9-239.3 0C3.8 175.9 0 168.4 0 159.2 0 115 13.9 74.2 39.1 44.1 63 15.7 94.6 0 128 0c33.5 0 65 15.7 88.9 44.1 25.2 30.1 39.1 70.9 39.1 115.1 0 9.2-3.7 16.7-8.3 16.7zM208.4 72.8C187 47.4 158.5 33.4 128 33.4 97.5 33.4 69 47.4 47.7 72.8 31.2 92.4 20.9 116.7 17.7 142.5l220.6 0C235.2 116.7 224.8 92.4 208.4 72.8Z"/></svg>
                 </div>
             </td>
@@ -57,7 +57,10 @@ export default {
       return positionKey === 'PK' && this.domes.includes(teamName)
     },
 
-    // todo: allow tiers of size 1
+    isInTag: function (tag, playerId) {
+      return this.tags.find(t => t.name === tag).players.find(p => p === playerId)
+    },
+
     isLastInTier: function (positionKey, index) {
       let teamsInLeague = this.teamsCount
       let positionTiers = (this.lastInTierIndexes.find(tier => tier.positionKey === positionKey) || {}).lastInTier
@@ -69,7 +72,7 @@ export default {
     },
 
     isTarget: function (playerId) {
-      return true
+      return this.isInTag('target', playerId)
     },
 
     playersByPosition: function (position) {
@@ -165,6 +168,7 @@ td:last-child {
 }
 
 .tagIconParent {
+  cursor: help;
   display: inline-block;
 }
 
