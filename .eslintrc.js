af40093f35338e345a7d0df0ef1b80b8c4a48c42
Brian Koser <brianmkoser@gmail.com>
2018-08-21 14:19:52 -0500

    Allow es6 backtick strings

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
+    // Allow single quote and backtick strings
+    quotes: ["error", "single", { "allowTemplateLiterals": true }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
