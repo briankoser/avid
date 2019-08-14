4f4113c3e13431a85670ee154b4ab3a11094fff1
Brian Koser <brianmkoser@gmail.com>
2019-08-14 16:27:48 -0500

    Set 2019 rankings, league settings

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -46,116 +46,116 @@ const state = {
     /*
       Family settings
     */
-    // league: {
-    //   draftOrderType: 'ser',
-    //   keepers: false, // issue #52
-    //   positions: [
-    //     { key: 'QB', min: 2, max: 3 },
-    //     { key: 'RB', min: 2, max: 6 },
-    //     { key: 'WR', min: 2, max: 6 },
-    //     { key: 'TE', min: 1, max: 6 },
-    //     { key: 'PK', min: 1, max: 3 },
-    //     { key: 'Def', min: 1, max: 3 }
-    //   ],
-    //   rosterSize: 16,
-    //   secondsPerPick: 60,
-    //   teams: [
-    //     { name: 'Force the Door', owner: 'Nathan Reimers', draftOrder: 12, keeper: '5848' },
-    //     { name: 'Tim Team', owner: 'Tim Binzel', draftOrder: 11, keeper: '13129' },
-    //     { name: 'Mile High Magic', owner: 'Melody Tijerina', draftOrder: 10, keeper: '13138' },
-    //     { name: 'Mom’s Marauders', owner: 'Sue Koser', draftOrder: 9, keeper: '13132' },
-    //     { name: 'Ya Ertz Me? Foles Reals', owner: 'Matt Koser', draftOrder: 8, keeper: '13604' },
-    //     { name: `Zach's What I'm Tolkien About`, owner: 'Zach Koser', draftOrder: 7, keeper: '7836' },
-    //     { name: 'I Say Kickaree Kee', owner: 'Brian Koser', draftOrder: 6, keeper: '9988', isUser: true },
-    //     { name: 'Kung Fu Manda', owner: 'Amanda Koser', draftOrder: 5, keeper: '11232' },
-    //     { name: 'Queens of the Forest', owner: 'Megan Hutt', draftOrder: 4, keeper: '12625' },
-    //     { name: 'Pensacola Pandas', owner: 'Josh Hutt', draftOrder: 3, keeper: '12150' },
-    //     { name: 'Melissa Team', owner: 'Melissa Koser', draftOrder: 2, keeper: '12171' },
-    //     { name: 'PApa Phillyman', owner: 'Don Koser', draftOrder: 1, keeper: '11192' }
-    //   ]
-    // },
-    // team: {
-    //   idealSize: [
-    //     { positionKey: 'QB', count: 3 },
-    //     { positionKey: 'RB', count: 5 },
-    //     { positionKey: 'WR', count: 5 },
-    //     { positionKey: 'TE', count: 1 },
-    //     { positionKey: 'PK', count: 1 },
-    //     { positionKey: 'Def', count: 1 }
-    //   ],
-    //   rankings: [],
-    //   tags: [
-    //     { name: 'avoid', players: ['0515', '9448', '9898', '11188', '11671', '12620', '12634', '13606', '13612'] },
-    //     { name: 'handcuff', players: ['10077', '10699', '11193', '11199', '11390', '13163', '13188', '13378', '13593', '13629'] },
-    //     { name: 'injuryprone', players: ['10722', '13608'] },
-    //     { name: 'overpriced', players: ['8670'] },
-    //     { name: 'target', players: ['8247', '9250', '9525', '9831', '9918', '10077', '10276', '10312', '10389', '10708', '10723', '10960', '10973', '11228', '11239', '11406', '11657', '11680', '11705', '11812', '11925', '12152', '12175', '12181', '12205', '12465', '12618', '12647', '12665', '13136', '13153', '13164', '13277', '13290', '13364', '13492', '13617', '13630', '13635', '13636', '13672'] }
-    //   ],
-    //   tiers: [
-    //     { positionKey: 'QB', sizes: [4, 5, 4, 5, 8, 6] },
-    //     { positionKey: 'RB', sizes: [3, 3, 5, 3, 11, 6] },
-    //     { positionKey: 'WR', sizes: [1, 7, 13, 8, 8, 7] },
-    //     { positionKey: 'TE', sizes: [3, 6, 3] },
-    //     { positionKey: 'PK', sizes: [] },
-    //     { positionKey: 'Def', sizes: [4, 3] }
-    //   ]
-    // }
-    /*
-      CGI settings
-    */
     league: {
       draftOrderType: 'ser',
-      keepers: false,
+      keepers: false, // issue #52
       positions: [
-        { key: 'QB', min: 1, max: 4 },
+        { key: 'QB', min: 2, max: 3 },
         { key: 'RB', min: 2, max: 6 },
         { key: 'WR', min: 2, max: 6 },
-        { key: 'TE', min: 1, max: 2 },
-        { key: 'PK', min: 1, max: 1 },
-        { key: 'Def', min: 1, max: 1 }
+        { key: 'TE', min: 1, max: 6 },
+        { key: 'PK', min: 1, max: 3 },
+        { key: 'Def', min: 1, max: 3 }
       ],
-      rosterSize: 14,
+      rosterSize: 16,
       secondsPerPick: 60,
       teams: [
-        { name: 'Prestige Worldwide', owner: 'Scott Hartwell', draftOrder: 1 },
-        { name: `Branden's Best Team`, owner: 'Branden Poe', draftOrder: 2 },
-        { name: `Tim's Mediocre Team`, owner: 'Tim Godfrey', draftOrder: 3 },
-        { name: 'Dom', owner: 'Dom Reese', draftOrder: 4 },
-        { name: 'Wentz Upon a Time', owner: 'Brian Koser', draftOrder: 5, isUser: true },
-        { name: 'The Alabama Randoms', owner: 'Josh Wainwright', draftOrder: 6 },
-        { name: 'Some Injured Guys', owner: 'Zac Smith', draftOrder: 7 },
-        { name: 'My Team Suxx!', owner: 'Steven Grimes', draftOrder: 8 },
-        { name: 'Suh-Tang Clan', owner: 'Luke Smith', draftOrder: 9 },
-        { name: 'Game of Jones', owner: 'Kristy King', draftOrder: 10 }
+        { name: 'Santa Clarita Ramily', owner: 'Mike Binzel', draftOrder: 12, keeper: '' },
+        { name: 'MileHigh Magic', owner: 'Melody Tijerina', draftOrder: 11, keeper: '' },
+        { name: 'The Thunder Thors', owner: 'Ian Bucy', draftOrder: 10, keeper: '' },
+        { name: 'Settlers of Saquon', owner: 'Matt Koser', draftOrder: 9, keeper: '13604' },
+        { name: 'Swing Low, Sweet Elliot', owner: 'Megan Hutt', draftOrder: 8, keeper: '' },
+        { name: 'Kung Fu Manda', owner: 'Amanda Koser', draftOrder: 7, keeper: '11232' },
+        { name: 'Honorable Shellback', owner: 'Tim Binzel', draftOrder: 6, keeper: '' },
+        { name: 'Turkish Delight', owner: 'Micah Bucy', draftOrder: 5, keeper: '' },
+        { name: 'PApa Phillyman', owner: 'Don Koser', draftOrder: 4, keeper: '' },
+        { name: 'Rig a Dee Dee', owner: 'Brian Koser', draftOrder: 3, keeper: '13130', isUser: true },
+        { name: `My Bois`, owner: 'Zach Koser', draftOrder: 2, keeper: '' },
+        { name: 'The Sun’ll Come Out Kamara', owner: 'Sue Koser', draftOrder: 1, keeper: '13132' }
       ]
     },
     team: {
       idealSize: [
         { positionKey: 'QB', count: 3 },
-        { positionKey: 'RB', count: 4 },
-        { positionKey: 'WR', count: 4 },
+        { positionKey: 'RB', count: 5 },
+        { positionKey: 'WR', count: 5 },
         { positionKey: 'TE', count: 1 },
         { positionKey: 'PK', count: 1 },
         { positionKey: 'Def', count: 1 }
       ],
       rankings: [],
       tags: [
-        { name: 'avoid', players: ['0515', '9448', '9898', '11188', '11671', '12620', '12634', '13606', '13612'] },
-        { name: 'handcuff', players: ['10077', '10699', '11193', '11199', '11390', '13163', '13188', '13378', '13593', '13629'] },
-        { name: 'injuryprone', players: ['10722', '13608'] },
-        { name: 'overpriced', players: ['8670'] },
-        { name: 'target', players: ['8247', '9250', '9525', '9831', '9918', '10077', '10276', '10312', '10389', '10708', '10723', '10960', '10973', '11228', '11239', '11406', '11657', '11680', '11705', '11812', '11925', '12152', '12175', '12181', '12205', '12465', '12618', '12647', '12665', '13136', '13153', '13164', '13277', '13290', '13364', '13492', '13617', '13630', '13635', '13636', '13672'] }
+        { name: 'avoid', players: ['4925','5848','7391','8062','9064','9075','9448','9925','9988','10261','10703','10722','10948','11192','11247','11670','11680','11695','12150','12151','12175','12181','12626','12676','13129','13146','13153','13192','13277','13299','13591','13629','14057','14058','14059'] },
+        { name: 'handcuff', players: ['11390','13404','13608','14223'] },
+        { name: 'injuryprone', players: ['11660','11761'] },
+        { name: 'overpriced', players: [''] },
+        { name: 'target', players: ['7394','9662','9918','10273','10276','10700','11248','11257','11516','11677','11747','11760','11886','12140','12187','12263','12620','12634','12647','12665','12677','13158','13163','13164','13234','13364','13404','13589','13590','13593','13605','13607','13612','13621','13633','13634','13635','13671','14056','14073','14079'] }
       ],
       tiers: [
         { positionKey: 'QB', sizes: [4, 5, 4, 5, 8, 6] },
-        { positionKey: 'RB', sizes: [3, 3, 5, 3, 11, 6] },
-        { positionKey: 'WR', sizes: [1, 7, 13, 8, 8, 7] },
-        { positionKey: 'TE', sizes: [3, 6, 3] },
+        { positionKey: 'RB', sizes: [4] },
+        { positionKey: 'WR', sizes: [6] },
+        { positionKey: 'TE', sizes: [1, 2, 2] },
         { positionKey: 'PK', sizes: [] },
-        { positionKey: 'Def', sizes: [4, 3] }
+        { positionKey: 'Def', sizes: [] }
       ]
     }
-  }
+    /*
+      CGI settings
+    */
+  //   league: {
+  //     draftOrderType: 'ser',
+  //     keepers: false,
+  //     positions: [
+  //       { key: 'QB', min: 1, max: 4 },
+  //       { key: 'RB', min: 2, max: 6 },
+  //       { key: 'WR', min: 2, max: 6 },
+  //       { key: 'TE', min: 1, max: 2 },
+  //       { key: 'PK', min: 1, max: 1 },
+  //       { key: 'Def', min: 1, max: 1 }
+  //     ],
+  //     rosterSize: 14,
+  //     secondsPerPick: 60,
+  //     teams: [
+  //       { name: 'Prestige Worldwide', owner: 'Scott Hartwell', draftOrder: 1 },
+  //       { name: `Branden's Best Team`, owner: 'Branden Poe', draftOrder: 2 },
+  //       { name: `Tim's Mediocre Team`, owner: 'Tim Godfrey', draftOrder: 3 },
+  //       { name: 'Dom', owner: 'Dom Reese', draftOrder: 4 },
+  //       { name: 'Wentz Upon a Time', owner: 'Brian Koser', draftOrder: 5, isUser: true },
+  //       { name: 'The Alabama Randoms', owner: 'Josh Wainwright', draftOrder: 6 },
+  //       { name: 'Some Injured Guys', owner: 'Zac Smith', draftOrder: 7 },
+  //       { name: 'My Team Suxx!', owner: 'Steven Grimes', draftOrder: 8 },
+  //       { name: 'Suh-Tang Clan', owner: 'Luke Smith', draftOrder: 9 },
+  //       { name: 'Game of Jones', owner: 'Kristy King', draftOrder: 10 }
+  //     ]
+  //   },
+  //   team: {
+  //     idealSize: [
+  //       { positionKey: 'QB', count: 3 },
+  //       { positionKey: 'RB', count: 4 },
+  //       { positionKey: 'WR', count: 4 },
+  //       { positionKey: 'TE', count: 1 },
+  //       { positionKey: 'PK', count: 1 },
+  //       { positionKey: 'Def', count: 1 }
+  //     ],
+  //     rankings: [],
+  //     tags: [
+  //       { name: 'avoid', players: ['0515', '9448', '9898', '11188', '11671', '12620', '12634', '13606', '13612'] },
+  //       { name: 'handcuff', players: ['10077', '10699', '11193', '11199', '11390', '13163', '13188', '13378', '13593', '13629'] },
+  //       { name: 'injuryprone', players: ['10722', '13608'] },
+  //       { name: 'overpriced', players: ['8670'] },
+  //       { name: 'target', players: ['8247', '9250', '9525', '9831', '9918', '10077', '10276', '10312', '10389', '10708', '10723', '10960', '10973', '11228', '11239', '11406', '11657', '11680', '11705', '11812', '11925', '12152', '12175', '12181', '12205', '12465', '12618', '12647', '12665', '13136', '13153', '13164', '13277', '13290', '13364', '13492', '13617', '13630', '13635', '13636', '13672'] }
+  //     ],
+  //     tiers: [
+  //       { positionKey: 'QB', sizes: [4, 5, 4, 5, 8, 6] },
+  //       { positionKey: 'RB', sizes: [3, 3, 5, 3, 11, 6] },
+  //       { positionKey: 'WR', sizes: [1, 7, 13, 8, 8, 7] },
+  //       { positionKey: 'TE', sizes: [3, 6, 3] },
+  //       { positionKey: 'PK', sizes: [] },
+  //       { positionKey: 'Def', sizes: [4, 3] }
+  //     ]
+  //   }
+  // }
 }
 
 const mutations = {
